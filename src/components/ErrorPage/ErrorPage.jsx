890e2ac5312483479074dbda7c5d1e198251cafd
mishap45 <pavlovmisha45@gmail.com>
2020-10-31 19:09:56 +0200

    Стилизировал компоненту ErrorPage.

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import style from './errorPage.module.css'
 
 const ErrorPage = () => {
-    return <div>404 NOT FOUND</div>
+    return <div className={style.errorMain}>
+        <p>404 NOT FOUND</p>
+    </div>
 };
 
 export default ErrorPage
